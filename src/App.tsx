f30b0f73c85008bc14ebe7eb6c68b4e2e14bcf9b
Elvan Hidayat <elvanhd@gmail.com>
2024-01-11 21:21:58 +0700

    Integrate Input component into App and utilize useRef
    - Implement Input component in App.tsx, replacing the Button and Container
    - Introduce useRef to obtain a reference to the input element
    - Refine Input component to support forwardRef for external usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import Button from './components/Button.tsx';
-import Container from './components/Container.tsx';
+import { useRef } from 'react';
+
+import Input from './components/Input.tsx';
 
 function App() {
+  const input = useRef<HTMLInputElement>(null);
+
   return (
     <main>
-      <Container as={Button}>Click me</Container>
+      <Input label='Test' id='test' ref={input} />
     </main>
   );
 }
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,15 +1,20 @@
-import { ComponentPropsWithoutRef } from 'react';
+import { forwardRef, type ComponentPropsWithoutRef } from 'react';
 
 type InputProps = {
   label: string;
   id: string;
 } & ComponentPropsWithoutRef<'input'>;
 
-export default function Input({ label, id, ...props }: InputProps) {
+const Input = forwardRef<HTMLInputElement, InputProps>(function Input(
+  { label, id, ...props },
+  ref,
+) {
   return (
     <p>
       <label htmlFor={id}>{label}</label>
-      <input id={id} {...props} />
+      <input id={id} {...props} ref={ref} />
     </p>
   );
-}
+});
+
+export default Input;
