554444615a48d7a572d250a12c9f2eea53e4b90a
Elvan Hidayat <elvanhd@gmail.com>
2024-01-11 21:28:52 +0700

    Implement Form component in App for data handling
    - Introduce Form component to encapsulate input fields and save functionality
    - Modify App to use Form and handleSave function for form submissions
    - Handle form submission with onSave callback and log extracted data

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import { useRef } from 'react';
-
+import Button from './components/Button.tsx';
+import Form from './components/Form.tsx';
 import Input from './components/Input.tsx';
 
 function App() {
-  const input = useRef<HTMLInputElement>(null);
+  function handleSave(data: unknown) {
+    const extractedData = data as { name: string; age: string };
+    console.log(extractedData);
+  }
 
   return (
     <main>
-      <Input label='Test' id='test' ref={input} />
+      <Form onSave={handleSave}>
+        <Input type='text' label='Name' id='name' />
+        <Input type='number' label='Age' id='age' />
+        <p>
+          <Button>Save</Button>
+        </p>
+      </Form>
     </main>
   );
 }
