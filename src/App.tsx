7be8c157ca48455fd6131df251170cc7bde6c847
Elvan Hidayat <elvanhd@gmail.com>
2024-01-11 21:11:28 +0700

    Enhance Container component to handle children and props
    - Modify ContainerProps to include 'children' and 'as' optional prop
    - Update App.tsx to showcase Container component with Button and text
    - Refine Container component to handle dynamic rendering and props

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Container from './components/Container.tsx';
 function App() {
   return (
     <main>
-      <Container as={Button} />
+      <Container as={Button}>Click me</Container>
     </main>
   );
 }
--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,10 +1,19 @@
-import { type ElementType } from 'react';
+import {
+  type ComponentPropsWithoutRef,
+  type ElementType,
+  type ReactNode,
+} from 'react';
 
-type ContainerProps = {
-  as: ElementType;
-};
+type ContainerProps<T extends ElementType> = {
+  as?: T;
+  children: ReactNode;
+} & ComponentPropsWithoutRef<T>;
 
-export default function Container({ as }: ContainerProps) {
-  const Component = as;
-  return <Component />;
+export default function Container<C extends ElementType>({
+  as,
+  children,
+  ...props
+}: ContainerProps<C>) {
+  const Component = as || 'div';
+  return <Component {...props}>{children}</Component>;
 }
