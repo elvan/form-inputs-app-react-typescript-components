1250e567865042eaaacc84ae38a54d8fbf2d7187
Elvan Hidayat <elvanhd@gmail.com>
2024-01-11 21:37:22 +0700

    Integrate form reset functionality in App
    - Introduce ref for customForm to access Form component methods
    - Implement handleSave to log extracted data and clear the form
    - Modify Form to expose clear method using useImperativeHandle

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
+import { useRef } from 'react';
+
 import Button from './components/Button.tsx';
-import Form from './components/Form.tsx';
+import Form, { type FormHandle } from './components/Form.tsx';
 import Input from './components/Input.tsx';
 
 function App() {
+  const customForm = useRef<FormHandle>(null);
+
   function handleSave(data: unknown) {
     const extractedData = data as { name: string; age: string };
     console.log(extractedData);
+    customForm.current?.clear();
   }
 
   return (
     <main>
-      <Form onSave={handleSave}>
+      <Form onSave={handleSave} ref={customForm}>
         <Input type='text' label='Name' id='name' />
         <Input type='number' label='Age' id='age' />
         <p>
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,10 +1,34 @@
-import { type ComponentPropsWithoutRef, type FormEvent } from 'react';
+import {
+  forwardRef,
+  useImperativeHandle,
+  useRef,
+  type ComponentPropsWithoutRef,
+  type FormEvent,
+} from 'react';
+
+export type FormHandle = {
+  clear: () => void;
+};
 
 type FormProps = ComponentPropsWithoutRef<'form'> & {
   onSave: (value: unknown) => void;
 };
 
-export default function Form({ onSave, children, ...otherProps }: FormProps) {
+const Form = forwardRef<FormHandle, FormProps>(function Form(
+  { onSave, children, ...otherProps },
+  ref,
+) {
+  const form = useRef<HTMLFormElement>(null);
+
+  useImperativeHandle(ref, () => {
+    return {
+      clear() {
+        console.log('CLEARING');
+        form.current?.reset();
+      },
+    };
+  });
+
   function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
@@ -14,8 +38,10 @@ export default function Form({ onSave, children, ...otherProps }: FormProps) {
   }
 
   return (
-    <form onSubmit={handleSubmit} {...otherProps}>
+    <form onSubmit={handleSubmit} {...otherProps} ref={form}>
       {children}
     </form>
   );
-}
+});
+
+export default Form;
